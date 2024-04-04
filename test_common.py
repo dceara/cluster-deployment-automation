e10889b46148a679ed0b025032d5dd5406d3fbbe
Thomas Haller <thaller@redhat.com>
2024-04-04 10:26:42 +0200

    common: add atomic_write() helper for writing files
    The "proper" way to write small configuration files is by writing them
    fully to a temporary file and do an atomic replace. Add a helper
    function for that.
    
    It's probably not perfect. It could work badly if you set certain user,
    group or mode, which prevent the function from completing. It might also
    not properly leave the right SELinux label (the proper solution might be
    that the temporary files get the appropriate label automatically). But
    especially because this isn't trivial, there is now one helper function
    that can attempt to get this right.

---
+++ test_common.py
@@ -0,0 +1,34 @@
+import pathlib
+import pytest
+import os
+
+import common
+
+
+def _read_file(filename: str) -> str:
+    with open(filename) as f:
+        return f.read()
+
+
+def test_atomic_write(tmp_path: pathlib.Path) -> None:
+
+    user = os.geteuid()
+    group = os.getegid()
+
+    filename = str(tmp_path / "file1")
+    with common.atomic_write(filename) as f:
+        f.write("hello1")
+    assert _read_file(filename) == "hello1"
+
+    filename = str(tmp_path / "file2")
+    with common.atomic_write(filename, mode=0o000) as f:
+        f.write("hello2")
+    assert os.path.exists(filename)
+    with pytest.raises(PermissionError):
+        # We took away permissions to read the file.
+        open(filename)
+
+    filename = str(tmp_path / "file3")
+    with common.atomic_write(filename, owner=user, group=group, mode=0o644) as f:
+        f.write("hello3")
+    assert _read_file(filename) == "hello3"
