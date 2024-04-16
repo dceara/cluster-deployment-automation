dceara/cluster-deployment-automation

commit c14c3c2bf8e3e543eedecf43ffc2d991808645b2

Add capability to snapshot clusters (VMs only for now)

clusterDeployer.py
@@ -26,15 +26,15 @@ from extraConfigCNO import ExtraConfigCNO
 from extraConfigRT import ExtraConfigRT
 import paramiko
 import common
-from virshPool import VirshPool
 import glob
 from logger import logger
 import logging
 
 
 def ensure_dhcp_entry(h: host.Host, name: str, ip: str, mac: str):
-    # If adding a worker node fails, one might want to retry w/o tearing down the whole cluster
-    # In that case, the DHCP entry might already be present, with wrong mac -> remove it
+    # If adding a worker node fails, one might want to retry w/o tearing down
+    # the whole cluster. In that case, the DHCP entry might already be present,
+    # with wrong mac -> remove it
 
     cmd = "virsh net-dumpxml default"
     ret = h.run_or_die(cmd)
@@ -50,17 +50,30 @@ def ensure_dhcp_entry(h: host.Host, name: str, ip: str, mac: str):
     h.run_or_die(cmd)
 
 
-def setup_vm(h: host.Host, virsh_pool: VirshPool, cfg: dict, iso_path: str):
+def setup_vm(h: host.Host, cfg: dict, iso_or_image_path: str):
     name = cfg["name"]
     ip = cfg["ip"]
     mac = "52:54:"+":".join(re.findall("..", secrets.token_hex()[:8]))
     ensure_dhcp_entry(h, name, ip, mac)
 
-    OS_VARIANT = "rhel8.5"
+    disk_size_gb = 48
+    if iso_or_image_path.endswith(".iso"):
+        options = "-o preallocation="
+        if cfg['sparse']:
+            options += "on"
+        else:
+            options += "off"
+
+        h.run(f'qemu-img create -f qcow2 {options} {cfg["image_path"]} {disk_size_gb}G')
+        cdrom_line = f"--cdrom {iso_or_image_path}"
+        append = "--wait=-1"
+    else:
+        cdrom_line = ""
+        append = "--noautoconsole"
+
+    OS_VARIANT = "rhel8.6"
     RAM_MB = 32784
-    DISK_GB = cfg['disk_size']
     CPU_CORE = 8
-    SPARSE = cfg['sparse']
     network = "default"
 
     cmd = f"""
@@ -74,34 +87,33 @@ def setup_vm(h: host.Host, virsh_pool: VirshPool, cfg: dict, iso_path: str):
         --import
         --network=network:{network},mac={mac}
         --events on_reboot=restart
-        --cdrom {iso_path}
-        --disk pool={virsh_pool.name()},size={DISK_GB},sparse={SPARSE},format=raw
-        --wait=-1
+        {cdrom_line}
+        --disk path={cfg["image_path"]}
+        {append}
     """
     logger.info(f"Starting VM {name}")
     ret = h.run(cmd)
     if ret.returncode != 0:
-        logger.info(f"Finished starting VM {name}, cmd = {cmd}, err=  error {ret}")
+        logger.info(f"Finished starting VM {name}, cmd = {cmd}, ret = {ret}")
     else:
         logger.info(f"Finished starting VM {name} without any errors")
     return ret
 
 
-def setup_all_vms(h: host.Host, vms, iso_path, virsh_pool) -> list:
+def setup_all_vms(h: host.Host, vms, iso_path) -> list:
     if not vms:
         return []
-
-    virsh_pool.ensure_initialized()
 
     executor = ThreadPoolExecutor(max_workers=len(vms))
     futures = []
     for e in vms:
-        futures.append(executor.submit(setup_vm, h, virsh_pool, e, iso_path))
+        futures.append(executor.submit(setup_vm, h, e, iso_path))
 
         while not h.vm_is_running(e["name"]) and not futures[-1].done():
             time.sleep(1)
 
     return futures
+
 
 def ensure_bridge_is_started(h: host.Host, bridge_xml: str):
     cmd = "virsh net-destroy default"
@@ -195,12 +207,6 @@ class ClusterDeployer():
         os.makedirs(self._iso_path, exist_ok=True)
         self._extra_config = ExtraConfigRunner(cc)
 
-        pool_name = f"{self._cc['name']}_guest_images"
-        for e in self._cc["hosts"]:
-            h = host.Host(e["name"])
-
-            e["virsh_pool"] = VirshPool(h, pool_name, e["images_path"])
-
         def empty():
             f = Future()
             f.set_result(None)
@@ -247,16 +253,15 @@ class ClusterDeployer():
         lh = host.LocalHost()
         for m in self._cc.local_vms():
             assert m["node"] == "localhost"
-            images_path = self.local_host_config()["virsh_pool"].images_path()
-            name = m["name"]
-            image = f"/{images_path}/{name}.img"
-            if os.path.exists(image):
-                os.remove(image)
-            image = f"/{images_path}/{name}.qcow2"
-            if os.path.exists(image):
-                os.remove(image)
+            # remove the image only if it really exists
+            image_path = m["image_path"]
+            if os.path.exists(image_path.replace(".qcow2", ".img")):
+                os.remove(image_path.replace(".qcow2", ".img"))
+            if os.path.exists(image_path):
+                os.remove(image_path)
 
-            # destroy the VM only if that really exists
+            # destroy the VM only if it really exists
+            name = m["name"]
             if lh.run(f"virsh desc {name}").returncode == 0:
                 r = lh.run(f"virsh destroy {name}")
                 logger.info(r.err if r.err else r.out.strip())
@@ -307,9 +312,6 @@ class ClusterDeployer():
                 f.write(json.dumps(filtered, indent=4))
             logger.info(lh.run("virsh net-start default"))
             logger.info(lh.run("systemctl restart libvirtd"))
-
-            vp = self.local_host_config()["virsh_pool"]
-            vp.ensure_removed()
 
         if self.need_api_network():
             intif = self._validate_api_port(lh)
@@ -505,8 +507,7 @@ class ClusterDeployer():
         # since self.local_host_config() is not present if no local vms
         if self._cc.local_vms():
             futures = setup_all_vms(lh, self._cc["masters"],
-                                    os.path.join(os.getcwd(), f"{infra_env}.iso"),
-                                    self.local_host_config()["virsh_pool"])
+                                    os.path.join(os.getcwd(), f"{infra_env}.iso"))
         else:
             self._create_physical_x86_nodes(self._cc["masters"])
             futures = []
@@ -524,7 +525,7 @@ class ClusterDeployer():
             p.result()
         self.ensure_linked_to_bridge()
         logger.info(f'downloading kubeconfig to {self._cc["kubeconfig"]}')
-        self._ai.download_kubeconfig(self._cc["name"], os.path.dirname(self._cc["kubeconfig"]))
+        self._ai.download_kubeconfig(self._cc["name"], self._cc["kubeconfig"])
         self._update_etc_hosts()
 
     def _print_logs(self, name):
@@ -588,7 +589,7 @@ class ClusterDeployer():
             rh = host.RemoteHost(ai_ip)
             rh.ssh_connect("core")
             rh.run("echo root:redhat | sudo chpasswd")
-        
+
         self._perform_worker_health_check(self._cc["workers"])
 
     def _create_physical_x86_nodes(self, nodes) -> None:
@@ -619,9 +620,7 @@ class ClusterDeployer():
         # TODO: clean this up. Currently just skipping this
         # since self.local_host_config() is not present if no local vms
         if self._cc.local_vms():
-            _ = setup_all_vms(lh, vm,
-                                     os.path.join(os.getcwd(), f"{infra_env}.iso"),
-                                     self.local_host_config()["virsh_pool"])
+            _ = setup_all_vms(lh, vm, os.path.join(os.getcwd(), f"{infra_env}.iso"))
         self._wait_known_state(e["name"] for e in vm)
 
     def _create_x86_workers(self) -> None:

virshPool.py
@@ -1,47 +1,0 @@
-from logger import logger
-import host
-
-
-class VirshPool:
-    def __init__(self, host: host.Host, name: str, images_path: str):
-        self._host = host
-        self._name = name
-        self._images_path = images_path
-
-    def name(self) -> str:
-        return self._name
-
-    def images_path(self) -> str:
-        return self._images_path
-
-    def initialized(self) -> bool:
-        cmd = f"virsh pool-info {self._name}"
-        return self._host.run(cmd).returncode == 0
-
-    def ensure_initialized(self) -> None:
-        if not self.initialized():
-            self.initialize()
-        else:
-            logger.info(f"Pool {self._name} already initialized")
-
-    def remove(self) -> None:
-        logger.info(self._host.run(f"virsh pool-destroy {self._name}"))
-        logger.info(self._host.run(f"virsh pool-undefine {self._name}"))
-
-    def ensure_removed(self) -> None:
-        if self.initialized():
-            self.remove()
-
-    def initialize(self) -> None:
-        def run_and_log(cmd):
-            r = self._host.run(cmd)
-            logger.debug(cmd)
-            if r.returncode:
-                logger.warn(f"Ran {cmd} and got error: {r}")
-
-        logger.info(f"Initializing pool {self._name} at {self._images_path}")
-        run_and_log(f"virsh pool-define-as {self._name} dir - - - - {self._images_path}")
-        run_and_log(f"mkdir -p {self._images_path}")
-        run_and_log(f"chmod a+rw {self._images_path}")
-        run_and_log(f"virsh pool-start {self._name}")
-        logger.info("Pool initialized")

arguments.py
@@ -6,21 +6,31 @@ import logging
 
 
 def parse_args() -> argparse.Namespace:
-    parser = argparse.ArgumentParser(description='Set up or tear down a (set of) clusters.')
+    parser = argparse.ArgumentParser(description='Cluster deployment automation')
     parser.add_argument('config', metavar='config', type=str, help='Yaml file with config')
-    parser.add_argument('-t', '--teardown', dest='teardown', action='store_true', help='Remove anything that would be created by setting up the cluster(s)')
-    parser.add_argument('-f', '--teardown-full', dest='teardown_full', action='store_true', help='Remove anything that would be created by setting up the cluster(s), included ai')
-    parser.add_argument('--assisted-installer-url', dest='url', default='192.168.122.1', action='store', type=str, help='If set to 0.0.0.0 (the default), Assisted Installer will be started locally')
-    parser.add_argument('--secret', dest='secrets_path', default='', action='store', type=str, help='pull_secret.json path (default is in cwd)')
     steps = "pre,masters,workers,post"
-    parser.add_argument('-s', '--steps', dest='steps', type=str, default=steps, help=f'Comma-separated list of steps to run (by default: {steps})')
-    parser.add_argument('-d', '--skip-steps', dest='skip_steps', type=str, default="", help=f"CommComma-separated list of steps to skip")
     parser.add_argument('-v', '--verbosity', choices=['debug', 'info', 'warning', 'error', 'critical'], default='info', help='Set the logging level (default: info)')
+    parser.add_argument('--secret', dest='secrets_path', default='', action='store', type=str, help='pull_secret.json path (default is in cwd)')
+    parser.add_argument('--assisted-installer-url', dest='url', default='192.168.122.1', action='store', type=str, help='If set to 0.0.0.0 (the default), Assisted Installer will be started locally')
+
+    subparsers = parser.add_subparsers(title='subcommands', dest='subcommand', required=True)
+    deploy_parser = subparsers.add_parser('deploy', help='Deploy clusters')
+    deploy_parser.add_argument('-t', '--teardown', dest='teardown', action='store_true', help='Remove anything that would be created by setting up the cluster(s)')
+    deploy_parser.add_argument('-f', '--teardown-full', dest='teardown_full', action='store_true', help='Remove anything that would be created by setting up the cluster(s), included ai')
+    steps = "pre,masters,workers,post"
+    deploy_parser.add_argument('-s', '--steps', dest='steps', type=str, default=steps, help=f'Comma-separated list of steps to run (by default: {steps})')
+    deploy_parser.add_argument('-d', '--skip-steps', dest='skip_steps', type=str, default="", help=f"CommComma-separated list of steps to skip")
+
+    snapshot_parser = subparsers.add_parser('snapshot', help='Take or restore snapshots')
+    snapshot_parser.add_argument('loadsave', metavar='loadsave', type=str, help='Load or save a snapshot')
+    snapshot_parser.add_argument('--name', type=str, default=None, help="Name of the snapshot (default is name of cluster)")
 
     args = parser.parse_args()
-    args.steps = args.steps.split(",")
-    args.skip_steps = args.skip_steps.split(",")
-    args.steps = [x for x in args.steps if x not in args.skip_steps]
+    if args.subcommand == "deploy":
+        args.steps = args.steps.split(",")
+        args.skip_steps = args.skip_steps.split(",")
+        args.steps = [x for x in args.steps if x not in args.skip_steps]
+
     log_levels = {
         'debug': logging.DEBUG,
         'info': logging.INFO,
@@ -33,7 +43,6 @@ def parse_args() -> argparse.Namespace:
 
     if not args.secrets_path:
         args.secrets_path = os.path.join(os.getcwd(), "pull_secret.json")
-
     if not os.path.exists(args.secrets_path):
         url = "https://console.redhat.com/openshift/install/pull-secret"
         logger.info(f"Missing secrets file at {args.secrets_path}, get it from {url}")

assistedInstaller.py
@@ -34,6 +34,10 @@ class AssistedClientAutomation(AssistedClient):
         if name in map(lambda x: x["name"], self.list_infra_envs()):
             self.delete_infra_env(name)
 
+    def download_kubeconfig(self, name, path, stdout=False):
+        path = os.path.dirname(path)
+        return super().download_kubeconfig(name, path, stdout)
+
     def download_iso_with_retry(self, infra_env: str):
         logger.info(self.info_iso(infra_env, {}))
         logger.info("Downloading iso (will retry if not ready)...")

assistedInstallerService.py
@@ -10,6 +10,7 @@ import sys
 import re
 from logger import logger
 import filecmp
+from typing import Optional
 
 
 def load_url_or_file(url_or_file: str):
@@ -40,7 +41,8 @@ that can be used to create and monitor clusters. However, since we are deploying
 non-standard way, the web-ui can't be used.
 """
 class AssistedInstallerService():
-    def __init__(self, ip: str, branch: str = "master"):
+    def __init__(self, version, ip: str, branch: str = "master"):
+        self._version = version
         self._ip = ip
         base_url = f"https://raw.githubusercontent.com/openshift/assisted-service/{branch}"
         pod_config_url = f"{base_url}/deploy/podman/configmap.yml"
@@ -49,10 +51,10 @@ class AssistedInstallerService():
         self.podFile = load_url_or_file(pod_file)
         self.workdir = os.path.join(os.getcwd(), "build")
 
-    def _configure(self, version) -> None:
+    def _configure(self) -> None:
         os.makedirs(self.workdir, exist_ok=True)
         with open(self._config_map_path(), 'w') as out_configmap:
-            yaml.dump(self._customized_configmap(version), out_configmap, sort_keys=False)
+            yaml.dump(self._customized_configmap(), out_configmap, sort_keys=False)
 
         with open(self._pod_persistent_path(), 'w') as out_pod:
             yaml.dump(self._customized_pod_persistent(), out_pod, default_flow_style=False)
@@ -69,7 +71,7 @@ class AssistedInstallerService():
     def _last_run_pod(self) -> str:
         return f'{self.workdir}/pod-persistent-last.yml'
 
-    def _customized_configmap(self, version):
+    def _customized_configmap(self):
         y = yaml.safe_load(self.podConfig)
         y["data"]["IMAGE_SERVICE_BASE_URL"] = f"http://{self._ip}:8888"
         y["data"]["SERVICE_BASE_URL"] = f"http://{self._ip}:8090"
@@ -88,7 +90,8 @@ class AssistedInstallerService():
         # and nightly) in the same config. For that reason, pass in the
         # version, and instantiate AI _only_ with one version, i.e. the
         # version we will be using.
-        y["data"]["RELEASE_IMAGES"] = json.dumps([self.prep_version(version)])
+        version_contents = self.prep_version(self._version)
+        y["data"]["RELEASE_IMAGES"] = json.dumps([version_contents])
         return y
 
     def _customized_pod_persistent(self) -> str:
@@ -183,53 +186,60 @@ class AssistedInstallerService():
     def get_normal_pullspec(self, version) -> str:
         return f"quay.io/openshift-release-dev/ocp-release:{version}-multi"
 
-    def ai_running(self) -> bool:
+    def find_pod(self, name) -> Optional[dict]:
         lh = host.LocalHost()
         result = lh.run("podman pod ps --format json")
         if result.err:
             logger.error(f"Error {result.err}")
             exit(1)
+        for x in json.loads(result.out):
+            if x["Name"] == name:
+                return x
+        return None
 
-        def expected(x):
-            return x["Name"], x["Status"] == "assisted-installer", "Running"
+    def pod_running(self) -> bool:
+        return bool(self.find_pod("assisted-installer"))
 
-        return any(expected(x) for x in json.loads(result.out))
+    def last_cm_is_same(self):
+        return os.path.exists(self._last_run_cm()) and filecmp.cmp(self._config_map_path(), self._last_run_cm())
 
-    def check_if_start_needed(self, force) -> bool:
-        lh = host.LocalHost()
+    def last_pod_is_same(self):
+        return os.path.exists(self._last_run_pod()) and filecmp.cmp(self._pod_persistent_path(), self._last_run_pod())
+
+    def stop_needed(self, force) -> bool:
         name = "assisted-installer"
-        if force and self.ai_running():
-            logger.info(f"{name} already running and stopping since force requested")
-            lh.run(f"podman pod stop {name}")
-            lh.run(f"podman pod rm {name}")
+        ai_pod = self.find_pod(name)
+        if not ai_pod:
+            logger.info(f"{name} not yet running")
+            return False
+
+        if force:
+            logger.info(f"{name} already running but force requested")
             return True
-        if self.ai_running():
-            cm_same = os.path.exists(self._last_run_cm()) and filecmp.cmp(self._config_map_path(), self._last_run_cm())
-            pod_same = os.path.exists(self._last_run_pod()) and filecmp.cmp(self._pod_persistent_path(), self._last_run_pod())
-            if cm_same and pod_same:
-                logger.info(f"{name} already running with the same configmap and pod config, will not restart it")
+        else:
+            if ai_pod["Status"] != "Running":
+                logger.info(f'{name} already exists but status is {ai_pod["Status"]}')
+                return True
+
+            if self.last_cm_is_same() and self.last_pod_is_same():
+                logger.info(f"{name} already running with the same configmap and pod config")
                 return False
             else:
-                logger.info(f"{name} already running with a different configmap, stopping it")
-                lh.run(f"podman pod stop {name}")
-                lh.run(f"podman pod rm {name}")
+                logger.info(f"{name} already running with a different configmap")
                 return True
-        else:
-            logger.info(f"{name} not yet running")
-            return True
 
     def _ensure_pod_started(self, force) -> None:
-        if self.check_if_start_needed(force):
+        if self.stop_needed(force):
+            self.stop()
+
+        if not self.pod_running():
             shutil.copy(self._config_map_path(), self._last_run_cm())
             shutil.copy(self._pod_persistent_path(), self._last_run_pod())
-            r = self._play_kube(self._last_run_cm(), self._last_run_pod())
-            if r.returncode != 0:
-                logger.error(r)
-                sys.exit(-1)
+            self._play_kube(self._last_run_cm(), self._last_run_pod())
 
     def _play_kube(self, cm, pod) -> host.Result:
         lh = host.LocalHost()
-        r = lh.run(f"podman play kube --configmap {cm} {pod}")
+        r = lh.run_or_die(f"podman play kube --configmap {cm} {pod}")
         return r
 
     def _ensure_libvirt_running(self) -> None:
@@ -268,20 +278,16 @@ class AssistedInstallerService():
 
     def stop(self) -> None:
         lh = host.LocalHost()
-        result = lh.run("podman pod ps --format json")
-        if result.err:
-            logger.error(f"Error {result.err}")
-            exit(1)
-        name = "assisted-installer"
-        if name in map(lambda x: x["Name"], json.loads(result.out)):
-            logger.info(f"{name} already running, stopping it before restarting")
-            lh.run(f"podman pod stop {name}")
-            lh.run(f"podman pod rm {name}")
-        else:
-            logger.debug(f"{name} not yet running")
+        ret = lh.run("podman pod ps --format json")
+        pod_name = "assisted-installer"
+        target = [x for x in json.loads(ret.out) if x["Name"] == pod_name]
+        if target:
+            logger.info(f"Stopping {pod_name}")
+            lh.run(f"podman pod stop {pod_name}")
+            lh.run(f"podman pod rm {pod_name}")
 
-    def start(self, version, force=False) -> None:
-        self._configure(version)
+    def start(self, force=False) -> None:
+        self._configure()
         self._ensure_pod_started(force)
         self.wait_for_api()
 
@@ -301,12 +307,19 @@ class AssistedInstallerService():
             export_vol(path, e)
 
     def import_snapshot(self, path) -> None:
+        self.stop()
         lh = host.LocalHost()
 
         def import_vol(path, vol_name):
+            nested = "rm -rf /source_data/*"
+            cmd = f"podman run -it --name snapshot --privileged -v {vol_name}:/source_data -v {path}:/export_data alpine sh -c '{nested}'"
+            lh.run("podman rm snapshot")
+            logger.info(lh.run(cmd))
+            logger.info(lh.run("podman rm snapshot"))
             nested = f"tar -xzf /export_data/{vol_name}.tar.gz -C /source_data"
             cmd = f"podman run -it --name snapshot --privileged -v {vol_name}:/source_data -v {path}:/export_data alpine sh -c '{nested}'"
             logger.info(cmd)
+            lh.run("podman rm snapshot")
             logger.info(lh.run(cmd))
             logger.info(lh.run("podman rm snapshot"))
 
@@ -314,3 +327,6 @@ class AssistedInstallerService():
         logger.info(f"importing {target} from {path}")
         for e in target:
             import_vol(path, e)
+
+        self.start(True)
+        time.sleep(30)

cda.py
@@ -4,15 +4,15 @@ from clustersConfig import ClustersConfig
 from clusterDeployer import ClusterDeployer
 from arguments import parse_args
 from logger import logger
+from clusterSnapshotter import ClusterSnapshotter
 
 
-def main():
-    args = parse_args()
+def main_deploy(args):
     cc = ClustersConfig(args.config)
 
     if args.url == "192.168.122.1":
-        ais = AssistedInstallerService(args.url)
-        ais.start(cc["version"])
+        ais = AssistedInstallerService(cc["version"], args.url)
+        ais.start()
         # workaround, this will still install 4.14, but AI will think
         # it is 4.13 (see also workaround when setting up versions)
         if cc["version"][:len("4.14")] == "4.14":
@@ -32,12 +32,36 @@ def main():
     ai = AssistedClientAutomation(f"{args.url}:8090")
     cd = ClusterDeployer(cc, ai, args, args.secrets_path)
 
-    if args.teardown or args.teardown_full:
+    if args.teardown:
         cd.teardown()
     else:
         cd.deploy()
-    if args.teardown_full:
-        ais.stop()
+
+
+def main_snapshot(args):
+    args = parse_args()
+    cc = ClustersConfig(args.config)
+
+    ais = AssistedInstallerService(cc["version"], args.url)
+    ai = AssistedClientAutomation(f"{args.url}:8090")
+
+    name = cc["name"] if args.name is None else args.name
+    cs = ClusterSnapshotter(cc, ais, ai, name)
+
+    if args.loadsave == "load":
+        cs.import_cluster()
+    elif args.loadsave == "save":
+        cs.export_cluster()
+    else:
+        logger.error(f"Unexpected action {args.actions}")
+
+
+def main():
+    args = parse_args()
+    if args.subcommand == "deploy":
+        main_deploy(args)
+    elif args.subcommand == "snapshot":
+        main_snapshot(args)
 
 
 if __name__ == "__main__":

commit c22162c8e8d3ffd0fbb3d51da32c06de9eef02bf

clusterDeployer: ensure virsh pool exists
Commit c14c3c2bf8e3 ('Add capability to snapshot clusters (VMs only for
now)') dropped the pool handling, maybe because virt-install's "--disk"
argument changed from "pool=" to "path=".

However, even with "--disk path=", libvirt will still create a pool.
As installation progresses in parallel, two virt-install instances may
decide that they need to create the pool. This can result to a failure:

  2024-04-08 05:16:54 INFO: Finished starting VM nicmodecluster-master-3, cmd =
          virt-install
              --connect qemu:///system
              -n nicmodecluster-master-3
              -r 32768
              --cpu host
              --vcpus 8
              --os-variant=rhel8.6
              --import
              --network network=default,mac=52:54:cf:41:cb:f6
              --events on_reboot=restart
              --cdrom /home/nicmodecluster_guests_images/nicmodecluster-x86_64.iso
              --disk path=/home/nicmodecluster_guests_images/nicmodecluster-master-3.qcow2
              --wait=-1
          , ret = (returncode: 1, error: ERROR    Validating install media '/home/nicmodecluster_guests_images/nicmodecluster-x86_64.iso' failed: Could not define storage pool: operation failed: pool 'nicmodecluster_guests_images' alrea>
  )

I think this is actually a bug in virt-install. When two instances try
in parallel to create the same pool, they should handle such race.
Anyway. It needs a workaround.

This brings back cf2f4f6c1a7e ('Avoid initializing multiple times the
same pool') and partly revers commit  c14c3c2bf8e3 ('Add capability to
snapshot clusters (VMs only for now)').

Fixes: c14c3c2bf8e3 ('Add capability to snapshot clusters (VMs only for now)')

clusterDeployer.py
@@ -24,6 +24,7 @@ import microshift
 from extraConfigRunner import ExtraConfigRunner
 from clusterHost import ClusterHost
 import dnsutil
+from virshPool import VirshPool
 
 
 def match_to_proper_version_format(version_cluster_config: str) -> str:
@@ -154,6 +155,14 @@ class ClusterDeployer:
                 f.write(json.dumps(filtered, indent=4))
             logger.info(self._local_host.hostconn.run("virsh net-start default"))
             logger.info(self._local_host.hostconn.run("systemctl restart libvirtd"))
+
+            image_paths = {os.path.dirname(n.image_path) for n in self._cc.local_vms()}
+            for image_path in image_paths:
+                vp = VirshPool(
+                    name=os.path.basename(image_path),
+                    host=self._local_host.hostconn,
+                )
+                vp.ensure_removed()
 
         for h in self._all_hosts:
             h.ensure_not_linked_to_network()

clusterHost.py
@@ -11,6 +11,7 @@ import host
 from clustersConfig import BridgeConfig, ClustersConfig, HostConfig, NodeConfig
 from clusterNode import ClusterNode, X86ClusterNode, VmClusterNode, BFClusterNode
 from virtualBridge import VirBridge
+from virshPool import VirshPool
 
 
 class ClusterHost:
@@ -88,6 +89,17 @@ class ClusterHost:
             logger.info(f"Copying {local_iso_path} to {self.hostconn.hostname()}:/{iso_path}")
             self.hostconn.copy_to(local_iso_path, iso_path)
             logger.debug(f"iso_path is now {iso_path} for {self.hostconn.hostname()}")
+
+        # Create the storage pools. virt-install would create them, however, if two
+        # concurrent instances of virt-install try to create the same pool, there
+        # is a failure (a bug in virt-install?).
+        for image_path in image_paths:
+            vp = VirshPool(
+                name=os.path.basename(image_path),
+                host=self.hostconn,
+                image_path=image_path,
+            )
+            vp.ensure_initialized()
 
     def configure_bridge(self) -> None:
         if not self.hosts_vms:

virshPool.py
@@ -1,0 +1,52 @@
+from typing import Optional
+
+import host
+from logger import logger
+
+
+class VirshPool:
+    def __init__(self, name: str, host: host.Host, *, image_path: Optional[str] = None):
+        self._host = host
+        self._name = name
+        self._image_path = image_path
+
+    def __str__(self) -> str:
+        return f"{self._name}@{self._host.hostname()}"
+
+    def name(self) -> str:
+        return self._name
+
+    def image_path(self) -> Optional[str]:
+        return self._image_path
+
+    def initialized(self) -> bool:
+        cmd = f"virsh pool-info {self._name}"
+        return self._host.run(cmd).returncode == 0
+
+    def ensure_initialized(self) -> None:
+        if not self.initialized():
+            self.initialize()
+        else:
+            logger.info(f"virsh-pool[{self}]: Pool {self._name} already initialized (image-path={self._image_path})")
+
+    def _host_run(self, cmd: str) -> None:
+        self._host.run(cmd)
+
+    def remove(self) -> None:
+        self._host_run(f"virsh pool-destroy {self._name}")
+        self._host_run(f"virsh pool-undefine {self._name}")
+
+    def ensure_removed(self) -> None:
+        if self.initialized():
+            self.remove()
+
+    def initialize(self) -> None:
+        if not self._image_path:
+            raise RuntimeError("The VirshPool is created without an image path and cannot be initialized")
+
+        logger.info(f"virsh-pool[{self}]: Initializing pool {self._name} at {self._image_path}")
+        self._host_run(f"virsh pool-define-as {self._name} dir - - - - {self._image_path}")
+        self._host_run(f"mkdir -p {self._image_path}")
+        self._host_run(f"chmod a+rw {self._image_path}")
+        self._host_run(f"virsh pool-start {self._name}")
+        logger.info(f"virsh-pool[{self}]: Pool initialized")